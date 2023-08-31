e5bf06b8c7cf01530acc1b50929c8af2902b25b4
Aleksandr Chusovitin <38918351+aleksandrchusovitin@users.noreply.github.com>
2023-08-31 22:30:58 +0700

    [6]: added calc game (#4)
    * [6]: added calc game
    
    * [6]: added core games. Refactoring
    
    ---------
    
    Co-authored-by: Aleksandr Chusovitin <aleksandr.chusovitin@dualbootpartners.com>

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 from brain_games.engine import core
-from brain_games.games import brain_even
+from brain_games.games import brain_calc
 
 
 def main():
-    core(brain_even)
+    core(brain_calc)
 
 
 if __name__ == "__main__":
